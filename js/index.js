71623f20137f49fc3cc87e497080b262c73ebf2f
ZhouJiyuan <anozjy@gmail.com>
2022-07-15 17:33:00 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,10 +15,10 @@ const list = [
 //     name: '洪磊',
 //     range: 1,
 //   },
-  {
-    name: '侯家麒',
-    range: 1,
-  },
+//   {
+//     name: '侯家麒',
+//     range: 1,
+//   },
   {
     name: '黄佳豪',
     range: 1,
@@ -27,10 +27,10 @@ const list = [
 //     name: '黄山',
 //     range: 1,
 //   },
-  {
-    name: '林泽毅',
-    range: 1,
-  },
+//   {
+//     name: '林泽毅',
+//     range: 1,
+//   },
 //   {
 //     name: '田佳亮',
 //     range: 1,
