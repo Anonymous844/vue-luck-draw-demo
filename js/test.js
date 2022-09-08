1169d69c6042beea49104dd8e4f554a22678a2a8
ZhouJiyuan <anozjy@gmail.com>
2022-09-08 17:14:08 +0800

    Update test.js

---
+++ js/test.js
@@ -0,0 +1,97 @@
+[
+  {
+    material_name: 'demo-button',
+    material_version: '0.0.0',
+    material_cn_name: 'DemoButton 按钮',
+    material_description: '按钮',
+    developer_email: 'shenjiaqi@myhexin.com',
+    designer_email: 'bingguanqi@myhexin.com',
+    support_types: [ 'web' ],
+    dsl_type: 'vue',
+    readme_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/49f08da3-c9f1-4fc4-b65e-1cfb4a54b715.md',
+    cover_url: 'https://www.baidu.com/test.png',
+    changelog_url: 'https://www.baidu.com/README.md',
+    meta: {
+      meta_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/863bcbce-55db-4b71-85ec-b775d9962492.json',
+      lowcode_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/af64bbdb-4035-4d4d-96de-6752d301fc51.json',
+      js_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.js/m/atom/fcb970e0-7f8f-4788-ba51-a2258a7adedd.js'
+    },
+    platform: [ 'common', 'lowcode' ]
+  },
+  {
+    material_name: 'demo-calendar',
+    material_version: '0.0.0',
+    material_cn_name: 'DemoCalendar 日历',
+    material_description: '日历',
+    developer_email: 'shenjiaqi@myhexin.com',
+    designer_email: 'bingguanqi@myhexin.com',
+    support_types: [ 'web' ],
+    dsl_type: 'vue',
+    readme_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/2536e7bf-be63-4ae1-ac8d-97e1e7febd45.md',
+    cover_url: 'https://www.baidu.com/test.png',
+    changelog_url: 'https://www.baidu.com/README.md',
+    meta: {
+      meta_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/be3d9260-dab4-4b7e-9ccb-62154c105f27.json',
+      lowcode_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/ec3684ec-8d52-40a6-999e-2b3e067ce1ff.json',
+      js_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.js/m/atom/60a55ba8-d4cd-496d-90c2-54d3ba4ba140.js'
+    },
+    platform: [ 'common', 'lowcode' ]
+  },
+  {
+    material_name: 'demo-icon',
+    material_version: '0.0.0',
+    material_cn_name: 'DemoIcon 图标',
+    material_description: '图标',
+    developer_email: 'shenjiaqi@myhexin.com',
+    designer_email: 'bingguanqi@myhexin.com',
+    support_types: [ 'web' ],
+    dsl_type: 'vue',
+    readme_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/ba5d61a2-f178-4b5b-ab59-154c3b54821b.md',
+    cover_url: 'https://www.baidu.com/test.png',
+    changelog_url: 'https://www.baidu.com/README.md',
+    meta: {
+      meta_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/bb05426c-43df-4e4c-ac5a-1635397edd4f.json',
+      lowcode_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/19666f8a-b632-40c7-8371-d12580ef2269.json',
+      js_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.js/m/atom/88e8ce25-c2a6-4eb7-a126-7a0e398833c9.js'
+    },
+    platform: [ 'common', 'lowcode' ]
+  },
+  {
+    material_name: 'demo-popup',
+    material_version: '0.0.0',
+    material_cn_name: 'DemoPopup 弹出层',
+    material_description: '弹出层',
+    developer_email: 'shenjiaqi@myhexin.com',
+    designer_email: 'bingguanqi@myhexin.com',
+    support_types: [ 'web' ],
+    dsl_type: 'vue',
+    readme_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/571568b7-9856-403a-99fe-6ff31c1fabc6.md',
+    cover_url: 'https://www.baidu.com/test.png',
+    changelog_url: 'https://www.baidu.com/README.md',
+    meta: {
+      meta_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/b092aeb6-5750-4a5c-a5ea-b90fa5ce4700.json',
+      lowcode_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/ebad68d8-f686-4eca-85dd-ffa689338c3a.json',
+      js_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.js/m/atom/949a5b0b-52eb-4ffc-8524-4f20a5eb60bd.js'
+    },
+    platform: [ 'common', 'lowcode' ]
+  },
+  {
+    material_name: 'demo-stocklist',
+    material_version: '0.0.1',
+    material_cn_name: 'DemoStockList 股票列表',
+    material_description: '股票列表',
+    developer_email: 'shenjiaqi@myhexin.com',
+    designer_email: 'bingguanqi@myhexin.com',
+    support_types: [ 'web' ],
+    dsl_type: 'vue',
+    readme_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/249c07a6-010c-471a-b6bd-cb83422849dd.md',
+    cover_url: 'https://www.baidu.com/test.png',
+    changelog_url: 'https://www.baidu.com/README.md',
+    meta: {
+      meta_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/04916c55-d49d-4e96-94a6-6e63ecb5bb69.json',
+      lowcode_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.file/m/atom/bbccd011-76c1-4054-adc5-5610e55de808.json',
+      js_url: '//s.thsi.cn/staticS3/mobileweb-upload-static-server.js/m/atom/67a0770b-ff6b-4998-9b9b-c8bb49ce0594.js'
+    },
+    platform: [ 'common', 'lowcode' ]
+  }
+]
